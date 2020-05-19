ac3f8d196c1041da2f0d46ece84ac2fc737e2dd4
Taichi Soma <izewfktvy533zjmn@gmail.com>
2020-05-20 00:24:04 +0900

    Delete build.sh

--- containers/boinc-client/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build -t raspi-k8s-rah-boinc-client:latest .
-docker tag raspi-k8s-rah-boinc-client:latest izewfktvy533zjmn/raspi-k8s-rah-boinc-client:latest
-docker push izewfktvy533zjmn/raspi-k8s-rah-boinc-client:latest
-
-exit 0
