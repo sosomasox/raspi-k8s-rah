4d898f5519d9b70de1ff5346136b7283c3fdf814
Taichi Soma <izewfktvy533zjmn@gmail.com>
2020-10-06 05:54:08 +0000

    Using a new project URL.

--- containers/boinc-client/scripts/entrypoint.sh
+++ containers/boinc-client/scripts/entrypoint.sh
@@ -5,12 +5,12 @@ sleep 30
 /usr/bin/boinccmd --read_cc_config
 
 if [ $ROSETTA_AT_HOME_ACCOUNT_KEY != "" ]; then
-    /usr/bin/boinccmd --project_attach http://boinc.bakerlab.org/rosetta/ $ROSETTA_AT_HOME_ACCOUNT_KEY
+    /usr/bin/boinccmd --project_attach https://boinc.bakerlab.org/rosetta/ $ROSETTA_AT_HOME_ACCOUNT_KEY
     sleep 120
     
     while [ `boinccmd --get_tasks | cat | wc -l` -le 2 ]
     do
-        /usr/bin/boinccmd --project http://boinc.bakerlab.org/rosetta/ update
+        /usr/bin/boinccmd --project https://boinc.bakerlab.org/rosetta/ update
         sleep 15
     done
     
