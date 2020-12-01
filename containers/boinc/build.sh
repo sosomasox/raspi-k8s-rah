752d2a1dd6030d254be94179d9f008bb9d5fb07f
Taichi Soma <izewfktvy533zjmn@gmail.com>
2020-12-01 14:07:45 +0000

    Update

--- containers/boinc/build.sh
+++ containers/boinc/build.sh
@@ -1,3 +1,3 @@
-docker build --no-cache -t boinc:arm64v8 .
-docker tag boinc:arm64v8 izewfktvy533zjmn/boinc:arm64v8
-docker push izewfktvy533zjmn/boinc:arm64v8
+docker build --no-cache -t boinc:arm64 .
+docker tag boinc:arm64 izewfktvy533zjmn/boinc:arm64
+docker push izewfktvy533zjmn/boinc:arm64
