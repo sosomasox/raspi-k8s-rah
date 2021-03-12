8605d44cee5004165009b4c8b39bc655687a5a5e
Taichi Soma <izewfktvy533zjmn@gmail.com>
2021-03-12 20:59:22 +0000

    change DockerHub id

--- containers/boinc-client/build.sh
+++ containers/boinc-client/build.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
 docker build --no-cache -t boinc-client:arm64 .
-docker tag boinc-client:arm64 izewfktvy533zjmn/boinc-client:arm64
-docker push izewfktvy533zjmn/boinc-client:arm64
+docker tag boinc-client:arm64 sosomasox/boinc-client:arm64
+docker push sosomasox/boinc-client:arm64
+
+exit 0
--- containers/boinc/build.sh
+++ containers/boinc/build.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
 docker build --no-cache -t boinc:arm64 .
-docker tag boinc:arm64 izewfktvy533zjmn/boinc:arm64
-docker push izewfktvy533zjmn/boinc:arm64
+docker tag boinc:arm64 sosomasox/boinc:arm64
+docker push sosomasox/boinc:arm64
+
+exit 0
